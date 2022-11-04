7b694a716ffb3406e714d27d2ca5d4a5a5f1b010
mahesh <gangurdemahesh9@gmail.com>
2022-11-04 22:17:17 +0530

    [mahesh]Add Program file

---
+++ Day14_Assingment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day14_Assingment
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Day 14 Assingment");
+
+               
+                LinkedList<int> my_list = new LinkedList<int>();
+   
+                my_list.AddFirst(70);
+                my_list.AddFirst(30);
+                my_list.AddFirst(56);
+            
+
+                
+                foreach (int data in my_list)
+                {
+                    Console.WriteLine(data);
+                }
+
+            }
+    }
+}
